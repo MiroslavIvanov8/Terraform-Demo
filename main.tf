0e70c2f89163281f0bf773505c9e39ea202d3404
MiroslavIvanov8 <ivanov8.work@gmail.com>
2024-12-06 09:31:51 +0200

    workflow 3 jobs test plan adn apply, created a gitignore.

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.12.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "StorageRG"
+    storage_account_name = "taskboardstoragemiro"
+    container_name       = "taskboardcontainer"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
